43c30792432210accccde7a4062a6edc0e18bafb
Ivan Kalita <i.kalita@csgo.com>
2017-03-12 15:58:20 +1000

    Add "Next" method tests.

--- test/list.cpp
+++ test/list.cpp
@@ -96,6 +96,62 @@ TEST(previous, filled_end) {
   ASSERT_EQ(4, pos) << "Pre-end position mismatch.";
 }
 
+TEST(next, empty_pre_first) {
+  List *list = createList();
+  int pre_first = list->Previous(list->First());
+  ASSERT_EQ(list->First(), list->Next(pre_first));
+}
+
+TEST(next, empty_first) {
+  List *list = createList();
+  ASSERT_EQ(1, list->Next(list->First()));
+}
+
+TEST(next, empty_end) {
+  List *list = createList();
+  ASSERT_EQ(1, list->Next(list->End()));
+}
+
+TEST(next, empty_pre_end) {
+  List *list = createList();
+  int pre_end = list->Previous(list->End());
+  ASSERT_EQ(list->End(), list->Next(pre_end));
+}
+
+TEST(next, empty_post_end) {
+  List *list = createList();
+  int post_end = list->Next(list->End());
+  EXPECT_THROW(list->Next(post_end), OutOfBounds*);
+}
+
+TEST(next, filled_pre_first) {
+  List *list = createFilledList();
+  int pre_first = list->Previous(list->First());
+  ASSERT_EQ(list->First(), list->Next(pre_first));
+}
+
+TEST(next, filled_first) {
+  List *list = createFilledList();
+  ASSERT_EQ(1, list->Next(list->First()));
+}
+
+TEST(next, filled_end) {
+  List *list = createFilledList(5);
+  ASSERT_EQ(6, list->Next(list->End()));
+}
+
+TEST(next, filled_pre_end) {
+  List *list = createFilledList();
+  int pre_end = list->Previous(list->End());
+  ASSERT_EQ(list->End(), list->Next(pre_end));
+}
+
+TEST(next, filled_post_end) {
+  List *list = createFilledList();
+  int post_end = list->Next(list->End());
+  EXPECT_THROW(list->Next(post_end), OutOfBounds*);
+}
+
 int main(int argc, char *argv[]) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
